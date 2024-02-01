7bad115ff25fb540e5f42120d4eab2dee4a3f118
Yuchen Wei <86708505+ez4bk@users.noreply.github.com>
2024-02-01 15:15:07 -0500

    Initial commit

---
+++ src/main/java/com/ez4bk/eztakeout/EzTakeOutApplication.java
@@ -0,0 +1,13 @@
+package com.ez4bk.eztakeout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EzTakeOutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EzTakeOutApplication.class, args);
+	}
+
+}
