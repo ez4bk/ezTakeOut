85935e7f430cddd319e5faaf618814f8a7464637
Yuchen Wei <86708505+ez4bk@users.noreply.github.com>
2024-02-28 18:42:12 -0500

    Add and configure JacksonObjectMapper to avoid inconsistent data formats between frontend and backend

--- src/main/java/com/ez4bk/eztakeout/config/WebMvcConfig.java
+++ src/main/java/com/ez4bk/eztakeout/config/WebMvcConfig.java
@@ -1,9 +1,14 @@
 package com.ez4bk.eztakeout.config;
 
+import com.ez4bk.eztakeout.common.JacksonObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+
+import java.util.List;
 
 @Slf4j
 @Configuration
@@ -13,6 +18,15 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
         log.info("Static resources mapping");
         registry.addResourceHandler("/employee/**").addResourceLocations("classpath:/employee/");
         registry.addResourceHandler("/customer/**").addResourceLocations("classpath:/customer/");
+    }
+
+    @Override
+    protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        log.info("Customize message converters");
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        converter.setObjectMapper(new JacksonObjectMapper());
+        converters.add(0, converter);
+        super.extendMessageConverters(converters);
     }
 
 }
