1a4feaeaa40f77fe08ea77479b4b9ae9c61c3988
Yuchen Wei <86708505+ez4bk@users.noreply.github.com>
2024-02-29 21:28:51 -0500

    Add table field autofill, use ThreadLocal to store global variable

--- src/main/java/com/ez4bk/eztakeout/entity/Employee.java
+++ src/main/java/com/ez4bk/eztakeout/entity/Employee.java
@@ -4,11 +4,13 @@ import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.TableField;
 import lombok.Data;
 
+import java.io.Serial;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
 @Data
 public class Employee implements Serializable {
+    @Serial
     private static final long serialVersionUID = 1L;
     private Long id;
     private String name;
@@ -18,7 +20,11 @@ public class Employee implements Serializable {
     private String sex;
     private String idNumber;
     private Integer status;
+
+    @TableField(fill = FieldFill.INSERT)
     private LocalDateTime createTime;
+
+    @TableField(fill = FieldFill.INSERT_UPDATE)
     private LocalDateTime updateTime;
 
     @TableField(fill = FieldFill.INSERT)
